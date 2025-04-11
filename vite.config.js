9063bd25795246a565a071fe6e5aaba5d003703e
Slendermanyshka <slendermanyshka@gmail.com>
2025-04-11 23:32:23 +0300

    upd

--- vite.config.js
+++ vite.config.js
@@ -1,48 +1,57 @@
 import { defineConfig } from 'vite';
-import { glob } from 'glob';
+import { resolve } from 'path';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        input: glob.sync('./src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
+export default defineConfig({
+  root: 'src',
+  base: '/goit-js-hw-09/',
+  build: {
+    outDir:  '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+           input: {
+        index: resolve(__dirname, 'src/index.html'),
+        gallery: resolve(__dirname, 'src/1-gallery.html'),
+        form: resolve(__dirname, 'src/2-form.html'),
+      },
+
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+        entryFileNames: chunkInfo => {
+          if (chunkInfo.name === 'commonHelpers') {
+            return 'commonHelpers.js';
+          }
+          return '[name].js';
+        },
+        assetFileNames: assetInfo => {
+          if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+            return '[name].[ext]';
+          }
+          return 'assets/[name]-[hash][extname]';
         },
       },
-      outDir: '../dist',
-      emptyOutDir: true,
     },
-    plugins: [
-      injectHTML(),
-      FullReload(['./src/**/**.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
-    ],
-  };
+  },
+  plugins: [
+    injectHTML(),
+    FullReload(['./src/**/*.html']),
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        SortCss({
+          sort: 'mobile-first',
+        }),
+      ],
+    },
+  },
+  optimizeDeps: {
+    exclude: ['fsevents'],
+  },
 });
